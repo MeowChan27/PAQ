7528fcf945bf1c4e638c696b5e21d12cca1454d3
prasanaa-laptop <vinga.prasanaa@gmail.com>
2022-12-17 22:52:04 +0100

    x

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,6 @@ module com.example.paq {
     requires javafx.controls;
     requires javafx.fxml;
 
-    requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
     requires com.almasb.fxgl.all;
 
