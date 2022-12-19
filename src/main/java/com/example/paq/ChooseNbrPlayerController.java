2ee4e2792bbb80b524321c855780c1d4ab1d4c19
Prasanaa <vinga.prasanaa@gmail.com>
2022-12-19 19:17:25 +0100

    update v1

--- src/main/java/com/example/paq/ChooseNbrPlayerController.java
+++ src/main/java/com/example/paq/ChooseNbrPlayerController.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -13,13 +14,24 @@ import javafx.stage.Stage;
 import com.example.paq.fr.isep.game7WonderArch.domain.Game;
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.Objects;
+import java.util.ResourceBundle;
 
-public class ChooseNbrPlayerController {
+public class ChooseNbrPlayerController implements Initializable {
 
     @FXML
     Slider slider;
+
     private Parent root;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        slider.setMajorTickUnit(1);
+        slider.setMinorTickCount(0);
+        slider.setShowTickLabels(true);
+        slider.setSnapToTicks(true);
+    }
 
     public void chooseNbrPlayerBtn(ActionEvent event){
         int nbrPlayer = (int) slider.getValue();
